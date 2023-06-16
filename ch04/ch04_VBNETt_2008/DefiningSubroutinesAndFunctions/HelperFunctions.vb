2b895019981baf2bda936dd9b1f5ccda00606ee2
David Stevenson <drstevenson1958@yahoo.com>
2023-06-16 14:07:43 -0400

    Chapter 4: VB 2008 Programming Constructs, Part II

---
+++ ch04/ch04_VBNETt_2008/DefiningSubroutinesAndFunctions/HelperFunctions.vb
@@ -0,0 +1,19 @@
+﻿Module HelperFunctions
+	' Subroutines have no return value.
+	Sub PrintMessage(ByVal msg As String)
+		Console.WriteLine(msg)
+	End Sub
+
+	' Functions have a return value.
+	Function Add(ByVal x As Integer, ByVal y As Integer) As Integer
+		'Add = x + y
+		Return x + y
+	End Function
+
+	' This will not compile if Option Strict is on
+	Function Test() As Integer     ' As Object assumed
+		Return 5
+	End Function
+	'Error BC30210	Option Strict On requires all Function, Property, And Operator declarations To have an 'As' clause.	
+
+End Module
