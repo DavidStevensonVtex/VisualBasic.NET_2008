3ae0c6acd7506bb9862c66bbb5ef0d5feedcb363
David Stevenson <drstevenson1958@yahoo.com>
2023-06-17 19:06:13 -0400

    Implement Simple Class Example

---
+++ ch05/ch05_VBNET_2008/SimpleClassExample/Car.vb
@@ -0,0 +1,13 @@
+﻿Public Class Car
+	' The "state" of the Car.
+	Public petName As String
+	Public currSpeed As Integer
+
+	' The functionality of the Car.
+	Public Sub PrintState()
+		Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed)
+	End Sub
+	Public Sub SpeedUp(ByVal delta As Integer)
+		currSpeed += delta
+	End Sub
+End Class
---
+++ ch05/ch05_VBNET_2008/SimpleClassExample/Program.vb
@@ -0,0 +1,17 @@
+Imports System
+
+Module Program
+	Sub Main(args As String())
+		Console.WriteLine("***** Fun with Class Types *****")
+		' Allocate and configure a Car object.
+		Dim myCar As New Car()
+		myCar.petName = "Sven"
+		myCar.currSpeed = 10
+
+		'' Speed up the car a few times and print out the new state.
+		For i As Integer = 0 To 10
+			myCar.SpeedUp(5)
+			myCar.PrintState()
+		Next
+	End Sub
+End Module
