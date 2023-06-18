e72a5c932203c1a14724cf707169e46a80c752fe
David Stevenson <drstevenson1958@yahoo.com>
2023-06-18 16:29:50 -0400

    The Inherits Keyword

---
+++ ch06/ch06_VBNET_2008/BasicInheritance/MiniVan.vb
@@ -0,0 +1,6 @@
+﻿' MiniVan derives from Car
+Public Class MiniVan
+	Inherits Car
+	' Here, we can now define members that are unique to
+	' the MiniVan type.
+End Class
--- ch06/ch06_VBNET_2008/BasicInheritance/Program.vb
+++ ch06/ch06_VBNET_2008/BasicInheritance/Program.vb
@@ -8,5 +8,15 @@ Module Program
 		myCar.Speed = 50
 		Console.WriteLine("My car is going {0} MPH", myCar.Speed)
 		Console.WriteLine()
+
+		Dim myVan As New MiniVan()
+		myVan.Speed = 10
+		Console.WriteLine("My van is going {0} MPH", myVan.Speed)
+		Console.WriteLine()
+
+		' Error! Cannot access private data of the parent from an object.
+		'myVan.currSpeed = 10
+		' Error BC30389   'BasicInheritance.Car.currSpeed' is not accessible in this context because it is 'Private'.
+
 	End Sub
 End Module
