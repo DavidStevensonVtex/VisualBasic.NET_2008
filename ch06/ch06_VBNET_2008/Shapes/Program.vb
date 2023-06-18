8bc4fdba0dae2d8a2daffa14a7ed89346711a9e4
David Stevenson <drstevenson1958@yahoo.com>
2023-06-18 19:04:10 -0400

    Building a Polymorphic Interface with MustOverride

---
+++ ch06/ch06_VBNET_2008/Shapes/Program.vb
@@ -0,0 +1,26 @@
+Imports System
+
+Module Program
+	Sub Main(args As String())
+		Console.WriteLine("***** Fun with Polymorphism *****")
+		Console.WriteLine()
+
+		Dim hex As New Hexagon("Beth")
+		hex.Draw()
+
+		Dim cir As New Circle("Cindy")
+		' Calls base class implementation!
+		cir.Draw()
+		Console.WriteLine()
+
+		' Make an array of Shape compatible objects.
+		Dim myShapes() As Shape = {New Hexagon(), New Circle(), New Hexagon("Mick"), New Circle("Beth"), New Hexagon("Linda")}
+
+		' Loop over each item and interact with the polymorphic interface.
+		For Each s As Shape In myShapes
+			s.Draw()
+		Next
+
+		Console.WriteLine()
+	End Sub
+End Module
