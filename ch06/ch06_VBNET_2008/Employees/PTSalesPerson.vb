424a35198ec046966b08b70275e8a99cafb0056e
David Stevenson <drstevenson1958@yahoo.com>
2023-06-18 17:02:14 -0400

    Adding a Sealed (NotInheritable) Class - PTSalesPerson

---
+++ ch06/ch06_VBNET_2008/Employees/PTSalesPerson.vb
@@ -0,0 +1,14 @@
+﻿Public NotInheritable Class PTSalesPerson
+	Inherits SalesPerson
+	Public Sub New()
+	End Sub
+	Public Sub New(ByVal fullName As String,
+				   ByVal empAge As Integer,
+				   ByVal empID As Integer,
+				   ByVal currPay As Single,
+				   ByVal ssn As String,
+				   ByVal numberOfSales As Integer)
+		MyBase.New(fullName, empAge, empID, currPay, ssn, numberOfSales)
+	End Sub
+
+End Class
