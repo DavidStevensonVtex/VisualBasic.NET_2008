3dacdbc9878a9a10578a6c97fa48dc8a245c855b
David Stevenson <drstevenson1958@yahoo.com>
2023-06-17 20:08:15 -0400

    Observing Constructor Flow

--- ch05/ch05_VBNET_2008/SimpleClassExample/Motorcycle.vb
+++ ch05/ch05_VBNET_2008/SimpleClassExample/Motorcycle.vb
@@ -9,18 +9,22 @@
 	' Remember, fields of a class automatically 
 	' are set to zero on our behalf
 	Public Sub New()
+		Console.WriteLine("In default constructor")
 	End Sub
 
 	' Our custom constructor
 	Public Sub New(ByVal intensity As Integer)
 		Me.New(intensity, "")
+		Console.WriteLine("In constructor taking an integer")
 	End Sub
 
 	Public Sub New(ByVal name As String)
 		Me.New(5, name)
+		Console.WriteLine("In constructor taking an String")
 	End Sub
 
 	Public Sub New(ByVal intensity As Integer, ByVal name As String)
+		Console.WriteLine("In master constructor")
 		If intensity > 10 Then
 			intensity = 10
 		End If
