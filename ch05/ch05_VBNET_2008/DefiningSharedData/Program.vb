b1d795540a91b05602bc37a8b339e7a51ffd4240
David Stevenson <drstevenson1958@yahoo.com>
2023-06-17 20:35:19 -0400

    Defining Shared Data

---
+++ ch05/ch05_VBNET_2008/DefiningSharedData/Program.vb
@@ -0,0 +1,24 @@
+Imports System
+
+Module Program
+	Sub Main(args As String())
+		Console.WriteLine("**** Fund with Shared Data *****")
+		Dim s1 As New SavingsAccount(50)
+		Dim s2 As New SavingsAccount(100)
+
+		' Get and Set interest rate at objet level.
+		Console.WriteLine("Interest Rate is: {0}", s1.GetInterestRateObj())
+		s2.SetInterestRateObj(0.08)
+
+		' Make new object; this does NOT "reste" the interest rate for this object.
+		Dim s3 As New SavingsAccount(10000.75)
+		Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate())
+		Console.WriteLine()
+
+		SavingsAccount.SetInterestRate(0.09)
+		Console.WriteLine("Interest Rate is: {0}", s1.GetInterestRateObj())
+		Console.WriteLine("Interest Rate is: {0}", s2.GetInterestRateObj())
+		Console.WriteLine("Interest Rate is: {0}", s3.GetInterestRateObj())
+
+	End Sub
+End Module
---
+++ ch05/ch05_VBNET_2008/DefiningSharedData/SavingsAccount.vb
@@ -0,0 +1,29 @@
+﻿' This class has a single piece of non-Shared data.
+Public Class SavingsAccount
+
+	Public currBalance As Double
+
+	' A Shared point of data.
+	Public Shared currInterestRate As Double = 0.04
+
+	Public Sub New(ByVal balance As Double)
+		currBalance = balance
+	End Sub
+
+	' Shared members to get/set interest rate.
+	Public Shared Sub SetInterestRate(ByVal newRate As Double)
+		currInterestRate = newRate
+	End Sub
+	Public Shared Function GetInterestRate() As Double
+		Return currInterestRate
+	End Function
+
+	' Instance members to get/set interest rate
+	Public Sub SetInterestRateObj(ByVal newRate As Double)
+		currInterestRate = newRate
+	End Sub
+	Public Function GetInterestRateObj() As Double
+		Return currInterestRate
+	End Function
+
+End Class
