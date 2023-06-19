4cd5dbfc2aada278fabfd31730463ef5f19b0039
David Stevenson <drstevenson1958@yahoo.com>
2023-06-19 16:18:24 -0400

    Multiple disposable objects in a Using statement

--- ch08/ch08_VBNET_2008/SimpleDispose/Program.vb
+++ ch08/ch08_VBNET_2008/SimpleDispose/Program.vb
@@ -3,7 +3,8 @@ Imports System
 Module Program
 	Sub Main(args As String())
 		Console.WriteLine("***** Fun with Dispose *****")
-		Using rw As New MyResourceWrapper
+		Using rw As New MyResourceWrapper,
+				rw2 As New MyResourceWrapper
 			' Use the object, Dispose() is automatically called.
 		End Using
 	End Sub
