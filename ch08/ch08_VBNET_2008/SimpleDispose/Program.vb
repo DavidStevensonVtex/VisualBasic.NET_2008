507550bbb940e4577bf994e83cee6388abd58b52
David Stevenson <drstevenson1958@yahoo.com>
2023-06-19 16:17:14 -0400

    The VB Using Keyword

--- ch08/ch08_VBNET_2008/SimpleDispose/Program.vb
+++ ch08/ch08_VBNET_2008/SimpleDispose/Program.vb
@@ -3,11 +3,8 @@ Imports System
 Module Program
 	Sub Main(args As String())
 		Console.WriteLine("***** Fun with Dispose *****")
-		Dim rw As New MyResourceWrapper
-		Try
-			' Call members of rw.
-		Finally
-			rw.Dispose()
-		End Try
+		Using rw As New MyResourceWrapper
+			' Use the object, Dispose() is automatically called.
+		End Using
 	End Sub
 End Module
