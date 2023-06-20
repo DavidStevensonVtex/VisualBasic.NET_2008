dc9ae074771e44ba23556f03cd5172e5c14568a0
David Stevenson <drstevenson1958@yahoo.com>
2023-06-20 15:27:40 -0400

    Defining Custom Interfaces

---
+++ ch09/ch09_VBNET_2008/CustomInterface/IPointy.vb
@@ -0,0 +1,8 @@
+﻿
+
+' This interface defines the behavior of "having points."
+Public Interface IPointy
+	' Implicitly public and abstract.
+	Function GetNumberOfPoints() As Byte
+	ReadOnly Property Points() As Byte
+End Interface
---
+++ ch09/ch09_VBNET_2008/CustomInterface/Program.vb
@@ -0,0 +1,6 @@
+Imports System
+
+Module Program
+	Sub Main(args As String())
+	End Sub
+End Module
