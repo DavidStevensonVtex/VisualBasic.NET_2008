e7e534d947a6a2addf799101cf939e407ee33401
David Stevenson <drstevenson1958@yahoo.com>
2023-06-20 15:34:12 -0400

    Implementing an Interface Triangle, Hexagon

---
+++ ch09/ch09_VBNET_2008/CustomInterface/Hexagon.vb
@@ -0,0 +1,21 @@
+﻿Public Class Hexagon
+	Inherits Shape
+	Implements IPointy
+
+	Public Sub New()
+	End Sub
+	Public Sub New(ByVal name As String)
+		MyBase.New(name)
+	End Sub
+
+	Public ReadOnly Property Points As Byte Implements IPointy.Points
+		Get
+			Return 6
+		End Get
+	End Property
+
+	Public Overrides Sub Draw()
+		Console.WriteLine("Drawing {0} the Hexagon", shapeName)
+	End Sub
+
+End Class
