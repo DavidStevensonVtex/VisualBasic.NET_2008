83d5b70c113c70fb15771d1a0ee444178a5a47f6
David Stevenson <drstevenson1958@yahoo.com>
2023-06-21 14:07:14 -0400

    Chapter 10: Collections, Generics, and Nullable Data Types
    ArrayList and Queue

---
+++ ch10/ch10_VBNET_2008/CollectionTypes/Car.vb
@@ -0,0 +1,43 @@
+﻿Public Class Car
+	Implements IComparable
+
+	Public Property CarID As Integer
+	Public Property CurrentSpeed As Integer
+	Public Property PetName As String
+
+	Public Sub New(ByVal name As String, ByVal currSp As Integer, ByVal id As Integer)
+		PetName = name
+		CurrentSpeed = currSp
+		Me.CarID = id
+	End Sub
+
+	Public Function CompareTo(obj As Object) As Integer Implements IComparable.CompareTo
+		Dim temp As Car = CType(obj, Car)
+		If Me.CarID < temp.CarID Then
+			Return -1
+		ElseIf Me.CarID > temp.CarID Then
+			Return 1
+		Else
+			Return 0
+		End If
+	End Function
+
+	' This helper class is used to sort an array of Cars by pet name.
+	Public Class PetNameComparer
+		Implements IComparer
+
+		Public Function Compare(x As Object, y As Object) As Integer Implements IComparer.Compare
+			Dim c1 As Car = CType(x, Car)
+			Dim c2 As Car = CType(y, Car)
+			Return String.Compare(c1.PetName, c2.PetName)
+		End Function
+	End Class
+
+	' Property to return the pet name comparer
+
+	Public Shared ReadOnly Property SortByPetName() As IComparer
+		Get
+			Return New PetNameComparer()
+		End Get
+	End Property
+End Class
---
+++ ch10/ch10_VBNET_2008/CollectionTypes/Program.vb
@@ -0,0 +1,62 @@
+Imports System
+
+Module Program
+	Sub Main(args As String())
+		ArrayListTest()
+		QueueTest()
+	End Sub
+	Sub ArrayListTest()
+		' Make ArrayList and add a range of Cars.
+		Dim carArrayList As New ArrayList()
+		carArrayList.AddRange(New Car() {New Car("Fred", 90, 10), New Car("Mary", 100, 50), New Car("MB", 190, 11)})
+		Console.WriteLine($"Items in carArrayList: {carArrayList.Count}")
+
+		For Each c As Car In carArrayList
+			Console.WriteLine($"Car pet name: {c.PetName}")
+		Next
+
+		' Insert New car
+		Console.WriteLine("-> Inserting new Car.")
+		carArrayList.Insert(2, New Car("TheNewCar", 0, 12))
+		Console.WriteLine($"Items in carArrayList: {carArrayList.Count}")
+
+		' Get the subobjects as an array.
+		Dim arrayOfCars As Object() = carArrayList.ToArray()
+		Dim i As Integer = 0
+
+		' Now iterate over array using While loop/Length property.
+		While i < arrayOfCars.Length
+			Console.WriteLine($"Car pet name: {CType(arrayOfCars(i), Car).PetName}")
+			i = i + 1
+		End While
+	End Sub
+
+	Sub WashCar(ByVal c As Car)
+		Console.WriteLine($"Cleaning {c.PetName}")
+	End Sub
+
+	Sub QueueTest()
+		Console.WriteLine()
+		Console.WriteLine("QueueTest")
+		' Make a Q with three items.
+		Dim carWashQ As New Queue
+		carWashQ.Enqueue(New Car("FirstCar", 0, 1))
+		carWashQ.Enqueue(New Car("SecondCar", 0, 2))
+		carWashQ.Enqueue(New Car("ThirdCar", 0, 3))
+
+		' Peek at first car in Q.
+		Console.WriteLine("First in Q is {0}", CType(carWashQ.Peek(), Car).PetName)
+
+		' Remove each item from Q
+		WashCar(CType(carWashQ.Dequeue(), Car))
+		WashCar(CType(carWashQ.Dequeue(), Car))
+		WashCar(CType(carWashQ.Dequeue(), Car))
+
+		' Try to de-Q again?
+		Try
+			WashCar(CType(carWashQ.Dequeue(), Car))
+		Catch ex As Exception
+			Console.WriteLine($"Error!! {ex.Message}")
+		End Try
+	End Sub
+End Module
