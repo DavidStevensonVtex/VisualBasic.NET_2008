5fddf798aff943b046475824ff5a9116c4a0158c
David Stevenson <drstevenson1958@yahoo.com>
2025-06-30 14:11:36 -0400

    Chapter 3: Main() As a Function (Not a Subroutine)

--- chapter03/FunWithMain/Program.vb
+++ chapter03/FunWithMain/Program.vb
@@ -1,11 +1,13 @@
 ﻿Public Class Program
-    Shared Sub Main(ByVal args As String())
+    Shared Function Main(ByVal args As String()) As Integer
         Console.WriteLine("***** Fun with Main() *****")
         ' Get command-line args
         Dim s As String
         For Each s In args
             Console.WriteLine("Arg: {0}", s)
         Next
-    End Sub
+        ' Return a value to the OS.
+        Return 0
+    End Function
 
 End Class
