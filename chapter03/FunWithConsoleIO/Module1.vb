6a60f6086338a50d3f4bf1b0766f8ab217354f09
David Stevenson <drstevenson1958@yahoo.com>
2025-06-30 14:20:55 -0400

    Chapter 3: Fund with Console IO

---
+++ chapter03/FunWithConsoleIO/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("**** Fun with Console IO")
+        ' Echo some information to the console.
+        Console.Write("Enter your name: ")
+        Dim s As String = Console.ReadLine()
+        Console.WriteLine("Hello, {0}", s)
+
+        Console.WriteLine("Enter your age: ")
+        s = Console.ReadLine()
+        Console.WriteLine("You are {0} years old", s)
+
+    End Sub
+
+End Module
