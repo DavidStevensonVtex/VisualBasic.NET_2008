bcd509b561862f438b870027ed2dfe014b4e966d
David Stevenson <drstevenson1958@yahoo.com>
2025-06-30 14:07:56 -0400

    Chapter 3: Processing Command-Line Arguments using System.Environment

---
+++ chapter03/FunWithMain/Program.vb
@@ -0,0 +1,12 @@
+﻿Public Class Program
+    Shared Sub Main()
+        Console.WriteLine("***** Fun with Main() *****")
+        ' Get command-line args
+        Dim args As String() = Environment.GetCommandLineArgs()
+        Dim s As String
+        For Each s In args
+            Console.WriteLine("Arg: {0}", s)
+        Next
+    End Sub
+
+End Class
