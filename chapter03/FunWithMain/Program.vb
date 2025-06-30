514aff7e4b51d2fb2978dd9e5488943c43a9ed1e
David Stevenson <drstevenson1958@yahoo.com>
2025-06-30 14:16:17 -0400

    Chapter 3: Displaying the logical drives of your system

--- chapter03/FunWithMain/Program.vb
+++ chapter03/FunWithMain/Program.vb
@@ -6,6 +6,12 @@
         For Each s In args
             Console.WriteLine("Arg: {0}", s)
         Next
+
+        ' List the drives on this machine
+        Dim drives As String() = Environment.GetLogicalDrives()
+        For Each drive In drives
+            Console.WriteLine("You have a drive named {0}.", drive)
+        Next
         ' Return a value to the OS.
         Return 0
     End Function
