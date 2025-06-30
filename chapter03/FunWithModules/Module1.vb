aabd3d2f81d21f09305bfe76ba9ce09045a903d2
David Stevenson <drstevenson1958@yahoo.com>
2025-06-30 13:43:50 -0400

    Chapter 3: Add FunWithModules project.

---
+++ chapter03/FunWithModules/Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        ' Show banner.
+        DisplayBanner()
+        ' Get User's name and say howday.
+        GreetUser()
+    End Sub
+
+    Sub DisplayBanner()
+        ' Get the current color of the console text.
+        Dim currColor As ConsoleColor = Console.ForegroundColor
+
+        ' Set text color to yellow.
+        Console.ForegroundColor = ConsoleColor.Yellow
+        Console.WriteLine("******* Welcome to FunWithModules")
+        Console.WriteLine("This simple program illustrates the role")
+        Console.WriteLine("of the Module type.")
+
+        ' Rest to previous color of your console text.
+        Console.ForegroundColor = currColor
+        Console.WriteLine()
+    End Sub
+
+    Sub GreetUser()
+        Dim userName As String
+        Console.WriteLine("Please enter your name: ")
+        userName = Console.ReadLine()
+        Console.WriteLine("Hello there {0}. Nice to meet you.", userName)
+    End Sub
+
+End Module
---
+++ chapter03/FunWithModules/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FunWithModules")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FunWithModules")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6254f547-82d4-4bc2-bde1-02eae4ba28a9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
