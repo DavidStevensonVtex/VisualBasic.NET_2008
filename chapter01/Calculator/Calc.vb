26c6b31daf628b0dd51ac7c5f6046250f5a58d0a
David Stevenson <drstevenson1958@yahoo.com>
2025-06-30 11:33:58 -0400

    Chapter 1: Calculator Example

---
+++ chapter01/Calculator/Calc.vb
@@ -0,0 +1,9 @@
+﻿' The VB 2008 Calculator
+Namespace CalculatorExample
+    Public Class Calc
+        Public Function Add(ByVal x As Integer, ByVal y As Integer) As Integer
+            Return x + y
+        End Function
+    End Class
+End Namespace
+
---
+++ chapter01/Calculator/Program.vb
@@ -0,0 +1,16 @@
+﻿Namespace CalculatorExample
+    Module Program
+
+        Public Sub Main()
+            Dim ans As Integer
+            Dim c As New Calc()
+            ans = c.Add(10, 84)
+
+            Console.WriteLine("10 + 84 is {0}.", ans)
+            Console.ReadLine()
+
+        End Sub
+
+    End Module
+End Namespace
+
