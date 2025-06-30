ddca24809c81706b78e146c90d5bdf707797bf80
David Stevenson <drstevenson1958@yahoo.com>
2025-06-30 14:30:22 -0400

    Chapter 3: Formatting Console Output

--- chapter03/FunWithConsoleIO/Module1.vb
+++ chapter03/FunWithConsoleIO/Module1.vb
@@ -11,6 +11,13 @@
         s = Console.ReadLine()
         Console.WriteLine("You are {0} years old", s)
 
+        ' Specify string placeholders and values to use at runtime.
+        Dim theInt As Integer = 90
+        Dim theDouble As Double = 9.99
+        Dim theBool As Boolean = True
+        Console.WriteLine("Value of theInt: {0}", theInt)
+        Console.WriteLine("theDouble is {0} and theBool is {1}.", theDouble, theBool)
+
     End Sub
 
 End Module
