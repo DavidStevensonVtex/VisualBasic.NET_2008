7828388976157ea14e825f49967d4ff261fa1265
David Stevenson <drstevenson1958@yahoo.com>
2025-06-30 14:10:10 -0400

    Chapter 3: Processing Command-Line Arguments with Main()

--- chapter03/FunWithMain/Program.vb
+++ chapter03/FunWithMain/Program.vb
@@ -1,8 +1,7 @@
 ﻿Public Class Program
-    Shared Sub Main()
+    Shared Sub Main(ByVal args As String())
         Console.WriteLine("***** Fun with Main() *****")
         ' Get command-line args
-        Dim args As String() = Environment.GetCommandLineArgs()
         Dim s As String
         For Each s In args
             Console.WriteLine("Arg: {0}", s)
