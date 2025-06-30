635b4d2d4844f95e06aaea1fa34924170b6a9527
David Stevenson <drstevenson1958@yahoo.com>
2025-06-30 11:39:32 -0400

    Chapter 1: Add Point Class Library and Class

---
+++ chapter01/Point/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Point")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Point")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("93273ef5-1f7a-4d4b-b739-5c0a84318dee")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ chapter01/Point/Point.vb
@@ -0,0 +1,14 @@
+﻿Public Structure Point
+    Public xPos As Integer
+    Public yPos As Integer
+
+    Public Sub New(ByVal x As Integer, ByVal y As Integer)
+        xPos = x
+        yPos = y
+    End Sub
+
+    Public Sub Display()
+        Console.WriteLine("({0}, {1])", xPos, yPos)
+    End Sub
+
+End Structure
