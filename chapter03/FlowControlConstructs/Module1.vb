a1084546c341d257d15748aa999de0f4c340bafd
David Stevenson <drstevenson1958@yahoo.com>
2025-07-01 10:57:04 -0400

    Chapter 3: Flow-Control Constructs: If/Then/ElseIf/Else Statement

--- chapter03/FlowControlConstructs/Module1.vb
+++ chapter03/FlowControlConstructs/Module1.vb
@@ -1,11 +1,19 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim userDone As Boolean
-        ' Gather user input to assign Boolean value...
+        Dim userOption As String
 
-        If userDone = True Then
-            Console.WriteLine("Thanks for running this app")
+        ' Read user option from command line.
+        userOption = Console.ReadLine()
+
+        If userOption = "GodMode" Then
+            Console.WriteLine("You will never die...cheater!")
+        ElseIf userOption = "FullLife" Then
+            Console.WriteLine("At the end, heh?")
+        ElseIf userOption = "AllAmmo" Then
+            Console.WriteLine("Now we can rock and roll!")
+        Else
+            Console.WriteLine("Unknown option...")
         End If
 
     End Sub
