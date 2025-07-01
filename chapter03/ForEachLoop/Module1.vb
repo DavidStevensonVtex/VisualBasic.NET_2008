a05a99f97175fd4ecdda6df133e01169ea46c34a
David Stevenson <drstevenson1958@yahoo.com>
2025-07-01 11:24:13 -0400

    Chapter 3: For/Each Loop

---
+++ chapter03/ForEachLoop/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myStrings() As String = {"Fun", "with", "VB 2008"}
+
+        For Each str As String In myStrings
+            Console.WriteLine(str)
+        Next
+
+    End Sub
+
+End Module
