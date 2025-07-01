a9f393ef0a75ed5c444790a83e0cc152de8bbe9b
David Stevenson <drstevenson1958@yahoo.com>
2025-07-01 11:16:10 -0400

    Chapter 3: The Select/Case Statement

---
+++ chapter03/SelectCaseStatement/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        ' Prompt user with choices.
+        Console.WriteLine("Welcome to the world of .NET")
+        Console.WriteLine("1 = C#  2 = C++/CLI  3 = VB 2008")
+        Console.Write("Please select your implementation language: ")
+
+        ' Get choice.
+        Dim s As String = Console.ReadLine()
+        Dim n As Integer = Integer.Parse(s)
+
+        ' Based on input, act accordingly...
+        Select Case n
+            Case Is = 1
+                Console.WriteLine("C# is all about managed code.")
+            Case Is = 2
+                Console.WriteLine("Maintaining a legacy system, are we?")
+            Case Is = 3
+                Console.WriteLine("VB 2008: Full OO capabilities...")
+            Case Else
+                Console.WriteLine("Well...good luck with that!")
+        End Select
+
+    End Sub
+
+End Module
