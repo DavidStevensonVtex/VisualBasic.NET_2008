e5dbd5add1e93d9690ba0dd7cc7b745829490ed1
David Stevenson <drstevenson1958@yahoo.com>
2025-07-01 10:51:48 -0400

    Chapter 3: Flow-Control Constructs: If/Then Statement

---
+++ chapter03/FlowControlConstructs/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim userDone As Boolean
+        ' Gather user input to assign Boolean value...
+
+        If userDone = True Then
+            Console.WriteLine("Thanks for running this app")
+        End If
+
+    End Sub
+
+End Module
---
+++ chapter03/FlowControlConstructs/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FlowControlConstructs")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FlowControlConstructs")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("25356410-84d7-48c9-87ae-c508b4a56b1b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
