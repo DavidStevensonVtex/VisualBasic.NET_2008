986662b4e36bccdae5852b62e40bf6b77604886c
David Stevenson <drstevenson1958@yahoo.com>
2025-07-01 11:18:32 -0400

    Chapter 3: Iteration Constructs: For/Next Loop

---
+++ chapter03/IterationConstructs/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        'Prints out the numbers 5 - 25, inclusive.
+        Dim i As Integer
+        For i = 5 To 25
+            Console.WriteLine($"Number is: {i}")
+        Next
+
+    End Sub
+
+End Module
