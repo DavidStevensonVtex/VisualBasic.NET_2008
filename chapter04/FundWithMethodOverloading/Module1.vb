9e9a2c531c31f2ba9fe458857b9bf5a36d404a77
David Stevenson <drstevenson1958@yahoo.com>
2025-07-01 13:35:19 -0400

    Chapter 4: Understanding Method Overloading

---
+++ chapter04/FundWithMethodOverloading/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+    Function Add(ByVal x As Integer, ByVal y As Integer) As Integer
+        Return x + y
+    End Function
+
+    Function Add(ByVal x As Double, ByVal y As Double) As Double
+        Return x + y
+    End Function
+    Function Add(ByVal x As Long, ByVal y As Long) As Long
+        Return x + y
+    End Function
+
+    Sub Main()
+        Console.WriteLine("***** Fun with Method Overloading *****")
+        ' Calls Integer version of Add
+        Console.WriteLine(Add(10, 10))
+
+        ' Calls Long Version of Add()
+        Console.WriteLine(Add(900000000000000000, 900000000000000000))
+
+        ' Calls Double version of Add()
+        Console.WriteLine(Add(4.3, 4.4))
+    End Sub
+
+End Module
---
+++ chapter04/FundWithMethodOverloading/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FundWithMethodOverloading")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FundWithMethodOverloading")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d9181e10-a962-46ef-9666-ca5bfb8c0ba8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
