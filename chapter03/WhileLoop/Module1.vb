5876bb497a535b480da81eea63127c3d8e0283d4
David Stevenson <drstevenson1958@yahoo.com>
2025-07-01 11:26:57 -0400

    Chapter 3: Do/While and Do/Until Looping Constructs

---
+++ chapter03/WhileLoop/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim j As Integer
+        While j < 20
+            Console.Write(j & ", ")
+            j += 1
+        End While
+
+    End Sub
+
+End Module
