d8ace1f503b00b85ad030deb8405afec10a1da99
David Stevenson <drstevenson1958@yahoo.com>
2025-07-01 11:31:17 -0400

    Chapter 3: The  With Construct

---
+++ chapter03/WithConstruct/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myStuff As New ArrayList()
+        myStuff.Add(100)
+        myStuff.Add("Hello")
+        Console.WriteLine($"Size is: {myStuff.Count}")
+
+        myStuff = New ArrayList()
+        With myStuff
+            .Add(100)
+            .Add("Hello")
+            Console.WriteLine($"Size is: { .Count}")
+        End With
+
+    End Sub
+
+End Module
---
+++ chapter03/WithConstruct/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WithConstruct")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WithConstruct")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("50ca49e0-8593-451b-bc1d-30b866761bdc")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
