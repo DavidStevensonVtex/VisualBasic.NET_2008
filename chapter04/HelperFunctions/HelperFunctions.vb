ffd65d9740a44fea03132718d6698baee5808cb8
David Stevenson <drstevenson1958@yahoo.com>
2025-07-01 12:27:02 -0400

    Chapter 4: Defining Subroutines and Functions

---
+++ chapter04/HelperFunctions/HelperFunctions.vb
@@ -0,0 +1,17 @@
+﻿Module HelperFunctions
+
+    ' Subroutines have no return value
+    Sub PrintMessage(ByVal msg As String)
+        Console.WriteLine(msg)
+    End Sub
+
+    ' Functions have a return value
+    Function Add(ByVal x As Integer, ByVal y As Integer) As Integer
+        Return x + y
+    End Function
+
+    Sub Main()
+
+    End Sub
+
+End Module
---
+++ chapter04/HelperFunctions/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("HelperFunctions")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("HelperFunctions")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d9daa47b-441f-495f-a6e3-1da807a39219")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
