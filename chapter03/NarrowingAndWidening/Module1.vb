0c472b814cc021be1ade110117afd18f44e28931
David Stevenson <drstevenson1958@yahoo.com>
2025-07-01 10:24:19 -0400

    Chapter 3: Narrowing (Explicit) and Widening (Implicit) Data Type Conversions

---
+++ chapter03/NarrowingAndWidening/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("***** The Amazing Addition Program *****")
+        Dim a As Short = 9
+        Dim b As Short = 10
+        Console.WriteLine("a + b = {0}", Add(a, b))
+
+    End Sub
+
+    Function Add(ByVal x As Integer, ByVal y As Integer) As Integer
+        Return x + y
+    End Function
+
+End Module
---
+++ chapter03/NarrowingAndWidening/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("NarrowingAndWidening")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("NarrowingAndWidening")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e887b986-b636-4d56-a0ab-4d6a0d5349a1")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
