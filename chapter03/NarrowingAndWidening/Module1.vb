9977270a75dbec74258a1c6a9bfe6e1ac7f00603
David Stevenson <drstevenson1958@yahoo.com>
2025-07-01 10:39:00 -0400

    Chapterr 3: Narrowing and Widening: Explicit Conversion

--- chapter03/NarrowingAndWidening/Module1.vb
+++ chapter03/NarrowingAndWidening/Module1.vb
@@ -4,10 +4,16 @@
         Console.WriteLine("***** The Amazing Addition Program *****")
         Dim a As Short = 30000
         Dim b As Short = 30000
-        Dim answer As Short = Add(a, b)
-        ' Implicit conversion from 'Integer' to 'Short'.
-        ' Option Strict On disallows implicit conversions from 'Integer' to 'Short'.
+        Dim answer As Integer = Add(a, b)
         Console.WriteLine("a + b = {0}", answer)
+
+        Dim myByte As Byte
+        Dim myInt As Integer = 200
+        ' This will no longer work:
+        'myByte = myInt
+
+        myByte = CByte(myInt)
+        Console.WriteLine("Value of myByte: {0}", myByte)
 
     End Sub
 
