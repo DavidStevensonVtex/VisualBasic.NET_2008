522e735cb026ebccb4f557e9455609cca1974785
David Stevenson <drstevenson1958@yahoo.com>
2025-07-03 13:59:15 -0400

    Chapter 4: Understanding VB 2008 Enumerations Controlling the Underlying STorage for an Enum Declaring and Using Enums

---
+++ chapter04/Enumerations/Module1.vb
@@ -0,0 +1,55 @@
+﻿Module Module1
+    ' A custom enumeration.
+    Enum EmpType
+        Manager         ' = 0
+        Grunt           ' = 1
+        Contractor      ' = 2
+        VicePresident   ' = 3
+    End Enum
+
+    ' Begin with 102
+    Enum EmpType2
+        Manager = 102
+        Grunt               ' = 103
+        Contractor          ' = 104
+        VicePresident       ' = 105
+    End Enum
+
+    ' Elements of an enumeration need not be sequential
+    Enum EmpType3
+        Manager = 10
+        Grunt = 1
+        Contractor = 100
+        VicePresident = 9
+    End Enum
+
+    ' Controlling the Underlying Storage for an Enum
+    Enum EmpType4 As Byte
+        Manager = 10
+        Grunt = 1
+        Contractor = 100
+        VicePresident = 9
+    End Enum
+
+    ' Enums as parameters
+    Sub AskForBonus(ByVal e As EmpType)
+        Select Case (e)
+            Case EmpType.Contractor
+                Console.WriteLine("You already get enough cash...")
+            Case EmpType.Grunt
+                Console.WriteLine("You have got to be kidding...")
+            Case EmpType.Manager
+                Console.WriteLine("How about stock options instead?")
+            Case EmpType.VicePresident
+                Console.WriteLine("VERY GOOD, Sir!")
+        End Select
+    End Sub
+
+    Sub Main()
+        Console.WriteLine("***** Fun with Enums *****")
+        ' Make a contractor type
+        Dim emp As EmpType = EmpType.Contractor
+        AskForBonus(emp)
+    End Sub
+
+End Module
---
+++ chapter04/Enumerations/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Enumerations")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Enumerations")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("40f3c0fd-f26c-4db0-b703-2afc2c307d47")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
