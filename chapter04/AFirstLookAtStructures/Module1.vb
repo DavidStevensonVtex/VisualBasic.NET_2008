36b5b0273f932f1162ce43d6faede85e7734a6a4
David Stevenson <drstevenson1958@yahoo.com>
2025-07-03 14:25:31 -0400

    Chapter 4: Introducing the VB 2008 Structure Type

---
+++ chapter04/AFirstLookAtStructures/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("***** A First Look at Structures *****")
+        ' Create an initial Point.
+        Dim myPoint As New Point()
+        myPoint.X = 349
+        myPoint.Y = 76
+        myPoint.Display()
+
+        ' Adjust the X and Y values
+        myPoint.Increment()
+        myPoint.Display()
+        Console.WriteLine($"Point in hex: {myPoint.PointAsHexString()}")
+    End Sub
+
+End Module
