e33bd6013b023a9c66070f28e77a92d6b063e4d1
David Stevenson <drstevenson1958@yahoo.com>
2025-07-03 15:58:21 -0400

    Chapter 5: Defining the Pillars of OOP The Role of Encapsulation Aggregation Example

---
+++ chapter05/AggregationExample/Program.vb
@@ -0,0 +1,25 @@
+﻿Public Class Radio
+    Public Sub PowerUp(ByVal turnOn As Boolean)
+        Console.WriteLine($"Radio on: {turnOn}")
+    End Sub
+End Class
+
+Public Class Car
+    ' Car "has-a" Radio
+    Private myRadio As New Radio()
+
+    Public Sub TurnOnRadio(ByVal onOff As Boolean)
+        ' Delegate call to inner object.
+        myRadio.PowerUp(onOff)
+    End Sub
+End Class
+
+Module Program
+
+    Sub Main()
+        ' Cal is forwarded to Radio internally
+        Dim viper As New Car()
+        viper.TurnOnRadio(False)
+    End Sub
+
+End Module
