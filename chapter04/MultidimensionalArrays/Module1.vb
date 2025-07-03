68a6267534d9424f0dc7d8394ea29c3c6f60eced
David Stevenson <drstevenson1958@yahoo.com>
2025-07-03 13:43:23 -0400

    Chapter 4 : Working with Multidimensional Arrays

---
+++ chapter04/MultidimensionalArrays/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        MultiDimArray()
+    End Sub
+
+    Sub MultiDimArray()
+        Console.WriteLine("=> Multidimensional arrays.")
+        Dim myMatrix(6, 6) As Integer ' Makes a 7x7 array
+        ' Populate array.
+        Dim k As Integer, j As Integer
+        For k = 0 To 6
+            For j = 0 To 6
+                myMatrix(k, j) = k * j
+            Next
+        Next
+
+        ' Show array
+        For k = 0 To 6
+            For j = 0 To 6
+                Console.Write($"{myMatrix(k, j),3}")
+            Next
+            Console.WriteLine()
+        Next
+        Console.WriteLine()
+    End Sub
+
+End Module
---
+++ chapter04/MultidimensionalArrays/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MultidimensionalArrays")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MultidimensionalArrays")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6819e165-2a01-4f22-9e3e-a2136d4291fa")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
