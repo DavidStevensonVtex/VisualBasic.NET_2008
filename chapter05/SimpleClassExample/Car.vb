e470a132d01164fd3455dad659c12d90cf686b64
David Stevenson <drstevenson1958@yahoo.com>
2025-07-03 14:37:22 -0400

    Chapter 5: Introducing the VB 2008 Class Type

---
+++ chapter05/SimpleClassExample/Car.vb
@@ -0,0 +1,15 @@
+﻿Public Class Car
+    ' The "state" of a Car.
+    Public petName As String
+    Public currSpeed As Integer
+
+    ' The functionality of the Car.
+    Public Sub PrintState()
+        Console.WriteLine($"{petName} is going {currSpeed} MPH.")
+    End Sub
+
+    Public Sub SpeedUp(ByVal delta As Integer)
+        currSpeed += delta
+    End Sub
+
+End Class
---
+++ chapter05/SimpleClassExample/Program.vb
@@ -0,0 +1,18 @@
+﻿Module Program
+
+    Sub Main()
+        Console.WriteLine("***** Fun with Class Types *****")
+        ' Allocate and configure a Car object
+        Dim myCar As New Car()
+        myCar.petName = "Sven"
+        myCar.currSpeed = 10
+
+        ' Speed up the car a few times and print out the new state
+        For i As Integer = 0 To 10
+            myCar.SpeedUp(5)
+            myCar.PrintState()
+        Next
+
+    End Sub
+
+End Module
