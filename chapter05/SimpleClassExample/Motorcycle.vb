91f3cdc1ff6fd7500b4893cd14d9ce2b253a2462
David Stevenson <drstevenson1958@yahoo.com>
2025-07-03 14:56:53 -0400

    Chapter 5: The Default Constructor Revisited

---
+++ chapter05/SimpleClassExample/Motorcycle.vb
@@ -0,0 +1,22 @@
+﻿Public Class Motorcycle
+    Public driverIntensity As Integer
+
+    Public Sub PopAWheely()
+        For i As Integer = 0 To driverIntensity
+            Console.WriteLine("Yeeeeee Haaaaaeewww!")
+        Next
+    End Sub
+
+    ' Put back the default constructor.
+    ' Remember, fields of a class automatically are set to a default value, 
+    ' so driverIntensity is set to zero on our behalf.
+
+    Public Sub New()
+    End Sub
+
+    ' Out custom constructor.
+    Public Sub New(ByVal intensity As Integer)
+        driverIntensity = intensity
+    End Sub
+
+End Class
