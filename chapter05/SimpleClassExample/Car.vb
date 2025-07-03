b2bec373257979ef2262ddf763b9ee333c3eeefa
David Stevenson <drstevenson1958@yahoo.com>
2025-07-03 14:53:10 -0400

    Chapter 5: Defining Custom Constructors

--- chapter05/SimpleClassExample/Car.vb
+++ chapter05/SimpleClassExample/Car.vb
@@ -1,4 +1,20 @@
 ﻿Public Class Car
+    ' A custom default constructor.
+    Public Sub New()
+        petName = "Chuck"
+        currSpeed = 10
+    End Sub
+
+    ' Here, currSpeed will receive the default value of an Integer(zero).
+    Public Sub New(ByVal pn As String)
+        petName = pn
+    End Sub
+
+    Public Sub New(ByVal pn As String, ByVal cs As Integer)
+        petName = pn
+        currSpeed = cs
+    End Sub
+
     ' The "state" of a Car.
     Public petName As String
     Public currSpeed As Integer
--- chapter05/SimpleClassExample/Program.vb
+++ chapter05/SimpleClassExample/Program.vb
@@ -1,6 +1,20 @@
 ﻿Module Program
 
     Sub Main()
+        ' Make a Car called Chuck going 10 MPH.
+        Dim chuck As New Car()
+        chuck.PrintState()
+
+        ' Make a Car called Mary going 0 MPH.
+        Dim mary As New Car("Mary")
+        mary.PrintState()
+
+        ' Make a Car called DAisy going 75 MPH.
+        Dim daisy As New Car("Daisy", 75)
+        daisy.PrintState()
+    End Sub
+
+    Sub MainOld()
         Console.WriteLine("***** Fun with Class Types *****")
         ' Allocate and configure a Car object
         Dim myCar As New Car()
