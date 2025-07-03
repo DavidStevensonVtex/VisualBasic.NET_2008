4aec3926503bf4cd66cc2c6491df6596f58a6f66
David Stevenson <drstevenson1958@yahoo.com>
2025-07-03 15:40:35 -0400

    Chapter 5: Defining Shared Data

--- chapter05/SharedMethods/Program.vb
+++ chapter05/SharedMethods/Program.vb
@@ -1,6 +1,20 @@
 ﻿Module Program
 
     Sub Main()
+        Console.WriteLine("***** Fun with Shared Data *****")
+        Dim s1 As New SavingsAccount(50)
+        Dim s2 As New SavingsAccount(100)
+
+        ' Get and Set interest rate at the object level.
+        Console.WriteLine($"Interest Rate is: {s1.GetInterestRate()}")
+        s2.SetInterestRate(0.08)
+
+        ' Make new object; this does NOT "reset" the interest rate for this object.
+        Dim s3 As New SavingsAccount(10000.75)
+        Console.WriteLine($"Interest Rate is: {s3.GetInterestRate()}")
+    End Sub
+
+    Sub Main2()
         Console.WriteLine("***** Shared Methods *****")
         For i As Integer = 0 To 5
             Console.WriteLine(Teenager.Complain())
