e7d5ef015d0693b268fb6163bbccc560724057f3
David Stevenson <drstevenson1958@yahoo.com>
2025-07-03 15:15:13 -0400

    Chapter 5: Observing Constructor Flow

--- chapter05/SimpleClassExample/Motorcycle.vb
+++ chapter05/SimpleClassExample/Motorcycle.vb
@@ -16,20 +16,25 @@
     ' Remember, fields of a class automatically are set to a default value, 
     ' so driverIntensity is set to zero on our behalf.
 
+    ' Constructors
     Public Sub New()
+        Console.WriteLine("In default c-tor")
     End Sub
 
     ' Out custom constructor.
     Public Sub New(ByVal intensity As Integer)
         Me.New(intensity, "")
+        Console.WriteLine("In c-tor taking an integer")
     End Sub
 
     Public Sub New(ByVal name As String)
         Me.New(5, name)
+        Console.WriteLine("In c-tor taking a String")
     End Sub
 
     ' This is the "master" constructor that does all the real work.
     Public Sub New(ByVal intensity As Integer, ByVal name As String)
+        Console.WriteLine("In master c-tor")
         If intensity > 10 Then
             intensity = 10
         End If
