3668cef5196878b777ee66deaf1e68f0e68690ef
David Stevenson <drstevenson1958@yahoo.com>
2025-07-03 15:27:45 -0400

    Chapter 5; Understanding the Shared Keyword Defining Shared Methods (and Fields)

---
+++ chapter05/SharedMethods/Program.vb
@@ -0,0 +1,19 @@
+﻿Module Program
+
+    Sub Main()
+        Console.WriteLine("***** Shared Methods *****")
+        For i As Integer = 0 To 5
+            Console.WriteLine(Teenager.Complain())
+        Next
+
+        ' VB-ism!
+        Dim bob As New Teenager()
+        For i As Integer = 0 To 5
+            ' Access of shared member, constant member, enum member or nested type through an instance;
+            ' qualifying expression will not be evaluated.
+            'Console.WriteLine(bob.Complain())
+        Next
+
+    End Sub
+
+End Module
