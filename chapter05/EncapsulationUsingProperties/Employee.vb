46df97745ee655cf9e97d26f35acf62b2cd68c6c
David Stevenson <drstevenson1958@yahoo.com>
2025-07-03 16:35:25 -0400

    Chapter 5: Encapsulation Using Properties

---
+++ chapter05/EncapsulationUsingProperties/Employee.vb
@@ -0,0 +1,26 @@
+﻿Public Class Employee
+    ' Using Automatic Properties
+    Public Property Name As String
+    Public Property ID As Integer
+    Public Property Pay As Single
+
+    ' Constructors
+    Public Sub New()
+    End Sub
+    Public Sub New(ByVal name As String, ByVal id As Integer, ByVal pay As Single)
+        Me.Name = name
+        Me.ID = id
+        Me.Pay = pay
+    End Sub
+
+    ' Methods.
+    Public Sub GiveBonus(ByVal amount As Single)
+        Pay += amount
+    End Sub
+    Public Sub DisplayStats()
+        Console.WriteLine($"Name: {Name}")
+        Console.WriteLine($"ID:   {ID}")
+        Console.WriteLine($"Pay:  {Pay}")
+    End Sub
+
+End Class
