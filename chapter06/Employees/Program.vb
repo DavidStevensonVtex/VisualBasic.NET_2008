8eaa76ed3ea99e6570faa41025f05fa1fff6edcf
David Stevenson <drstevenson1958@yahoo.com>
2025-07-08 13:02:56 -0400

    Chapter 6: Controlling Base Class Creation with MyBase

--- chapter06/Employees/Program.vb
+++ chapter06/Employees/Program.vb
@@ -1,6 +1,10 @@
 ﻿Module Program
 
     Sub Main()
+        ' Assume we now have the following constructor for the Manager.
+        ' (name, age, ID, pay, SSN, number of stock options).
+        Dim chucky As New Manager("Chucky", 45, 101, 30000, "222-22-2222", 90)
+
 
     End Sub
 
