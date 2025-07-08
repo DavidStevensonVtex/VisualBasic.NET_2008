36cefe56fe9b3701100bc92c330538545f0b34da
David Stevenson <drstevenson1958@yahoo.com>
2025-07-08 12:27:33 -0400

    Chapter 5: Documenting VB 2008 Source Code via XML

---
+++ chapter05/DocumentingSourceCode/Employee.vb
@@ -0,0 +1,21 @@
+﻿''' <summary>
+''' This is the employee class.
+''' </summary>
+''' <remarks></remarks>
+Public Class Employee
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="Name"></param>
+    ''' <param name="age"></param>
+    ''' <param name="id"></param>
+    ''' <param name="pay"></param>
+    ''' <param name="ssn"></param>
+    Public Sub New(ByVal Name As String, ByVal age As Integer, ByVal id As Integer,
+        ByVal pay As Single, ByVal ssn As String)
+
+    End Sub
+
+
+End Class
---
+++ chapter05/DocumentingSourceCode/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DocumentingSourceCode")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DocumentingSourceCode")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d1834722-bf0d-49fe-bed2-d6c835a0f78c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
