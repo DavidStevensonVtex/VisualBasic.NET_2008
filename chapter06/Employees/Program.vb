090db0afffd2d4203ab70a6364b0624b54f5e796
David Stevenson <drstevenson1958@yahoo.com>
2025-07-08 13:15:23 -0400

    Chapter 6: Programming for Containment Delegates

--- chapter06/Employees/Program.vb
+++ chapter06/Employees/Program.vb
@@ -4,7 +4,8 @@
         ' Assume we now have the following constructor for the Manager.
         ' (name, age, ID, pay, SSN, number of stock options).
         Dim chucky As New Manager("Chucky", 45, 101, 30000, "222-22-2222", 90)
-
+        Dim cost As Double = chucky.GetBenefitCost()
+        Console.WriteLine($"Chucky's benefit cost: {cost:C}")
 
     End Sub
 
