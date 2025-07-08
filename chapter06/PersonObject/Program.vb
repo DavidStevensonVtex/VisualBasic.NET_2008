6b8d79add2fb5ebe9744a645d6af973bd9dd0c3f
David Stevenson <drstevenson1958@yahoo.com>
2025-07-08 15:00:50 -0400

    Chapter 6: Testing Our Modified Person Class

--- chapter06/PersonObject/Program.vb
+++ chapter06/PersonObject/Program.vb
@@ -3,6 +3,31 @@ Module Program
 
     Sub Main()
         Console.WriteLine("***** Fun with System.Object *****")
+        Dim p1 As New Person("Homer", "Simpson", 50)
+        Dim p2 As New Person("Homer", "Simpson", 50)
+
+        ' Get stringified version of objects
+        Console.WriteLine($"p1.ToString() = {p1.ToString()}")
+        Console.WriteLine($"p2.ToString() = {p2.ToString()}")
+
+        ' Test Overridden Equals
+        Console.WriteLine($"p1 = p2?: {p1.Equals(p2)}")
+
+        ' Test hash codes.
+        Console.WriteLine($"Same hash codes?: {p1.GetHashCode} {p2.GetHashCode}")
+        Console.WriteLine()
+
+        ' Change age of p2 and test again.
+        p2.personAge = 45
+        Console.WriteLine($"p1.ToString() = {p1.ToString()}")
+        Console.WriteLine($"p2.ToString() = {p2.ToString()}")
+        Console.WriteLine($"p1 = p2?: {p1.Equals(p2)}")
+        Console.WriteLine($"Same hash codes?: {p1.GetHashCode = p2.GetHashCode}")
+
+    End Sub
+
+    Sub OldMain()
+        Console.WriteLine("***** Fun with System.Object *****")
         Dim p1 As New Person("David", "Stevenson", 67)
 
         ' Use inherited members of System.Object
