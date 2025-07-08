4674f32e9010b94895ab6b0bb9cdcdc9859f4cea
David Stevenson <drstevenson1958@yahoo.com>
2025-07-08 12:44:24 -0400

    Chapter 6: The Inherits Keyword

---
+++ chapter06/BasicInheritance/MiniVan.vb
@@ -0,0 +1,12 @@
+﻿' MiniVan derives from Car.
+Public Class MiniVan
+    Inherits Car
+    ' He, we can now define members that are unique to
+    ' the MiniVan type.
+
+    Public Sub TestMethod()
+        ' OK! Can use public Car memebers within the derived type.
+        Speed = 10
+    End Sub
+
+End Class
--- chapter06/BasicInheritance/Program.vb
+++ chapter06/BasicInheritance/Program.vb
@@ -6,6 +6,11 @@
         Dim myCar As New Car(80)
         myCar.Speed = 50
         Console.WriteLine("My car is going {0} MPH", myCar.Speed)
+
+        ' Now createa  MiniVan object
+        Dim myVan As New MiniVan()
+        myVan.Speed = 10
+        Console.WriteLine("MyVan is going {0} MPH", myVan.Speed)
     End Sub
 
 End Module
