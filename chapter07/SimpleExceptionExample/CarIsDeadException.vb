4621b249033a0e14ee0cc00bf566791b8ddf7846
David Stevenson <drstevenson1958@yahoo.com>
2025-07-09 13:50:57 -0400

    Chapter 7: Building Custom Exceptions, Take Three

--- chapter07/SimpleExceptionExample/CarIsDeadException.vb
+++ chapter07/SimpleExceptionExample/CarIsDeadException.vb
@@ -1,9 +1,18 @@
-﻿Public Class CarIsDeadException
+﻿Imports System.Runtime.Serialization
+
+<Serializable()>
+Public Class CarIsDeadException
     Inherits ApplicationException
 
     Public Sub New()
     End Sub
     Public Sub New(ByVal msg As String)
         MyBase.New(msg)
+    End Sub
+    Public Sub New(ByVal message As String, ByVal inner As System.Exception)
+        MyBase.New(message, inner)
+    End Sub
+    Protected Sub New(ByVal info As SerializationInfo, ByVal context As StreamingContext)
+        MyBase.New(info, context)
     End Sub
 End Class
