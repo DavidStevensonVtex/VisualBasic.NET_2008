72d673f4f073b459abb3aff77b84b618a4f95c32
David Stevenson <drstevenson1958@yahoo.com>
2025-07-09 13:13:26 -0400

    Chapter 7: Catching Exceptions

--- chapter07/SimpleExceptionExample/Program.vb
+++ chapter07/SimpleExceptionExample/Program.vb
@@ -6,10 +6,19 @@
         Dim myCar As New Car("Zippy", 20)
         myCar.CrankTunes(True)
 
-        For i As Integer = 0 To 10
-            myCar.Accelerate(10)
-        Next
-        Console.ReadLine()
+        ' Try/Catch logic.
+        Try
+            For i As Integer = 0 To 10
+                myCar.Accelerate(10)
+            Next
+        Catch ex As Exception
+            Console.WriteLine("*** Error! ***")
+            Console.WriteLine($"Method: {ex.TargetSite}")
+            Console.WriteLine($"Message: {ex.Message}")
+            Console.WriteLine($"Source: {ex.Source}")
+        End Try
+
+        Console.WriteLine("**** Out of exception logic *****")
 
     End Sub
 
