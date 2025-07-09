e3cdfcbcb5b1e2e72b7646676a801bf612316764
David Stevenson <drstevenson1958@yahoo.com>
2025-07-09 13:06:03 -0400

    Chapter 7: Exceptions: The Simplest Possible Example

---
+++ chapter07/SimpleExceptionExample/Car.vb
@@ -0,0 +1,42 @@
+﻿Public Class Car
+    ' Constant for maximum speed
+    Public Const maxSpeed As Integer = 100
+    ' Internal state data
+    Private currSpeed As Integer
+    Private petName As String
+
+    ' Is the car still operational?
+    Private carIsDead As Boolean
+
+    ' A car has a radio
+    Private theMusicBox As New Radio()
+
+    ' Constructors
+    Public Sub New()
+    End Sub
+    Public Sub New(ByVal name As String, ByVal currSp As Integer)
+        currSpeed = currSp
+        petName = name
+    End Sub
+
+    Public Sub CrankTunes(ByVal state As Boolean)
+        theMusicBox.TurnOn(state)
+    End Sub
+
+    ' See if Car has overheated.
+    Public Sub Accelerate(ByVal delta As Integer)
+        If carIsDead Then
+            Console.WriteLine($"{petName} is out of order...")
+        Else
+            currSpeed += delta
+            If currSpeed > maxSpeed Then
+                Console.WriteLine($"{petName} has overheated!")
+                currSpeed = 0
+                carIsDead = True
+            Else
+                Console.WriteLine($"=> CurrSpeed = {currSpeed}")
+            End If
+        End If
+    End Sub
+
+End Class
---
+++ chapter07/SimpleExceptionExample/Program.vb
@@ -0,0 +1,16 @@
+﻿Module Program
+
+    Sub Main()
+        Console.WriteLine("**** Simple Exception Example *****")
+        Console.WriteLine("=> Creating a car and stepping on it!")
+        Dim myCar As New Car("Zippy", 20)
+        myCar.CrankTunes(True)
+
+        For i As Integer = 0 To 10
+            myCar.Accelerate(10)
+        Next
+        Console.ReadLine()
+
+    End Sub
+
+End Module
