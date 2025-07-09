dd93851461e6bdbae76a860204a45bd5b75014f9
David Stevenson <drstevenson1958@yahoo.com>
2025-07-09 13:45:37 -0400

    Chapter 7: Building Custom Exceptions, Take Two

--- chapter07/SimpleExceptionExample/CarIsDeadException.vb
+++ chapter07/SimpleExceptionExample/CarIsDeadException.vb
@@ -1,21 +1,9 @@
 ﻿Public Class CarIsDeadException
     Inherits ApplicationException
 
-    ' Default error message
-    Private messageDetails As String = "Car Error"
-
-    ' Constructors.
     Public Sub New()
     End Sub
     Public Sub New(ByVal msg As String)
-        messageDetails = msg
+        MyBase.New(msg)
     End Sub
-
-    ' Override the Exception.Message property.
-    Public Overrides ReadOnly Property Message() As String
-        Get
-            Return $"Car Error Message: {messageDetails}"
-        End Get
-    End Property
-
 End Class
