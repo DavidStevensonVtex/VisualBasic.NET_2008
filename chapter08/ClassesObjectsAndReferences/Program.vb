6cf02fc4eb11c7f1e06f2ad1eedb86acfdecb4f0
David Stevenson <drstevenson1958@yahoo.com>
2025-07-10 13:05:44 -0400

    Chapter 8: Classes, Objects and References

---
+++ chapter08/ClassesObjectsAndReferences/Program.vb
@@ -0,0 +1,15 @@
+﻿Module Program
+
+    Sub Main()
+        ' Createa new Car object on the managed heap. 
+        ' We are returned a reference to this object that
+        ' we store in the 'refToMyCar" local variable.
+        Dim refToMyCar As New Car("Zippy", 50)
+
+        ' The VB 2008 dot operator (.) is used to invoke 
+        ' members on the object using our reference variable.
+        Console.WriteLine(refToMyCar.ToString())
+
+    End Sub
+
+End Module
