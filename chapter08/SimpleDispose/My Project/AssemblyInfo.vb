331e0580c6e78cc1cf33523e342ec40e0e5cd96d
David Stevenson <drstevenson1958@yahoo.com>
2025-07-10 14:25:16 -0400

    Chapter 8: Building Disposable Objects, The VB Using Keyword

---
+++ chapter08/SimpleDispose/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SimpleDispose")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SimpleDispose")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("85f922b4-5515-4945-b4ad-a0087d531b72")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ chapter08/SimpleDispose/Program.vb
@@ -0,0 +1,20 @@
+﻿Module Program
+
+    Sub Main()
+        Console.WriteLine("***** Fun with Dispose ******")
+        'Dim rw As New MyResourceWrapper()
+        'Try
+        '    ' Call members of rw.
+        'Finally
+        '    rw.Dispose()
+        'End Try
+
+        Using rw As New MyResourceWrapper(),
+                rw2 As New MyResourceWrapper(),
+                myCar As New Car()
+            ' Use the objects, Dispose() automatically called!
+        End Using
+
+    End Sub
+
+End Module
