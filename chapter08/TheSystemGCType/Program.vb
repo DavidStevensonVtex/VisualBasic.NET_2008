00335b97277259c8efac0848286f4d0380efb745
David Stevenson <drstevenson1958@yahoo.com>
2025-07-10 13:34:42 -0400

    Chapter 8: The System.GC Type

---
+++ chapter08/TheSystemGCType/Program.vb
@@ -0,0 +1,18 @@
+﻿Module Program
+
+    Sub Main()
+        Console.WriteLine("***** Fun with System.GC *****")
+        ' Print out estimated number of bytes on heap.
+        Console.WriteLine("Estimated bytes on heap: {0}", GC.GetTotalMemory(False))
+
+        ' MaxGeneration is zero based, so add 1 for display purposes.
+        Console.WriteLine($"This OS has {GC.MaxGeneration + 1} generations.")
+
+        Dim refToMyCar As New Car("Zippy", 100)
+        Console.WriteLine(refToMyCar.ToString())
+        ' Print out generation of refToMyCar object.
+        Console.WriteLine($"Generation of refToMyCar is {GC.GetGeneration(refToMyCar)}")
+
+    End Sub
+
+End Module
