3db7a9eabe16ae5f32ce3bf22b24b388ece88fc8
David Stevenson <drstevenson1958@yahoo.com>
2025-07-12 15:28:14 -0400

    Chapter 9: INteracting with Types Supporting Interfaces

--- chapter09/CustomInterface/Program.vb
+++ chapter09/CustomInterface/Program.vb
@@ -1,6 +1,9 @@
 ﻿Module Program
 
     Sub Main()
+        Console.WriteLine("***** Fun with Interfaces *****")
+        Dim hex As New Hexagon()
+        Console.WriteLine($"Number of Point: {hex.Points}")
 
     End Sub
 
