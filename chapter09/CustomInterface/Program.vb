cc402e0da0b6f85d4364d7d1ea521eaf5ca31a16
David Stevenson <drstevenson1958@yahoo.com>
2025-07-12 15:32:27 -0400

    Chapter 9: Obtaining Interface References Using CType()

--- chapter09/CustomInterface/Program.vb
+++ chapter09/CustomInterface/Program.vb
@@ -5,6 +5,16 @@
         Dim hex As New Hexagon()
         Console.WriteLine($"Number of Point: {hex.Points}")
 
+        ' Recall, Circle does not support IPointy
+        Dim c As New Circle()
+        Dim itfPointy As IPointy
+        Try
+            itfPointy = CType(c, IPointy)
+            Console.WriteLine($"Number of Points: {itfPointy.Points}")
+        Catch ex As Exception
+            Console.WriteLine($"{c} does not implement IPointy!")
+        End Try
+
     End Sub
 
 End Module
