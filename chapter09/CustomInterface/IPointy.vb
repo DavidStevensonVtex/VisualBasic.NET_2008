52023d509a4ca684a194087c5fdfb1051f432bc5
David Stevenson <drstevenson1958@yahoo.com>
2025-07-12 15:19:19 -0400

    Chapter 9: Implementing the Interface

--- chapter09/CustomInterface/IPointy.vb
+++ chapter09/CustomInterface/IPointy.vb
@@ -1,6 +1,6 @@
 ﻿' This interface defines the behavior of "having points."
 Public Interface IPointy
     ' Implicitly public and abstract.
-    Function GetNumberOfPoints() As Byte
+    'Function GetNumberOfPoints() As Byte
     ReadOnly Property Points() As Byte
 End Interface
