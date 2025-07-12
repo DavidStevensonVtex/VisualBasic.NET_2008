15a18c28b28a8d9b30573f05dab356c8dd64c59f
David Stevenson <drstevenson1958@yahoo.com>
2025-07-12 15:40:12 -0400

    Chapter 9: Obtaining Interface References Using TypeOf/Is

--- chapter09/CustomInterface/Program.vb
+++ chapter09/CustomInterface/Program.vb
@@ -18,6 +18,20 @@
         ' See which objects support IPointy.
         Console.WriteLine($"Circle implements IPointy? {TypeOf c Is IPointy}")
         Console.WriteLine($"Hexagon implements IPointy?: {TypeOf hex Is IPointy}")
+
+        ' Make an array of Shape-compatible types.
+        Dim myShapes() As Shape = {New Hexagon("Fred"), New Circle("Angie"),
+            New ThreeDCircle(), New Triangle("Adam")}
+        ' Now figure out which ones support IPointy
+        'Dim itfPointy As IPointy
+        For Each s As Shape In myShapes
+            If TypeOf s Is IPointy Then
+                itfPointy = CType(s, IPointy)
+                Console.WriteLine($"{s.PetName} has {itfPointy.Points} points.")
+            Else
+                Console.WriteLine($"{s} does not implement IPointy !")
+            End If
+        Next
     End Sub
 
 End Module
