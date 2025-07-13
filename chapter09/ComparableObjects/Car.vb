a77e74094c4015e4503b6e2fd452111ed1bb498d
David Stevenson <drstevenson1958@yahoo.com>
2025-07-13 15:28:13 -0400

    Chapter 9: Building Comparable Objects (IComparable)

---
+++ chapter09/ComparableObjects/Car.vb
@@ -0,0 +1,30 @@
+﻿Public Class Car
+    Implements IComparable
+
+    Public Property Name As String
+    Public Property Speed As Integer
+    Public Property ID As Integer
+
+    Public Sub New()
+    End Sub
+    Public Sub New(ByVal name As String, ByVal speed As Integer, ByVal id As Integer)
+        Me.Name = name
+        Me.Speed = speed
+        Me.ID = id
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return $"{Name} is going {Speed} MPH"
+    End Function
+
+    Public Function CompareTo(obj As Object) As Integer Implements IComparable.CompareTo
+        Dim temp As Car = CType(obj, Car)
+        If Me.ID > temp.ID Then
+            Return 1
+        ElseIf Me.ID < temp.ID Then
+            Return -1
+        Else
+            Return 0
+        End If
+    End Function
+End Class
---
+++ chapter09/ComparableObjects/Program.vb
@@ -0,0 +1,32 @@
+﻿Module Program
+
+    Sub Main()
+        Console.WriteLine("***** Fun with IComparable *****")
+        ' Make an array of Car objects
+        Dim myAutos(4) As Car
+        myAutos(0) = New Car("Rusty", 80, 1)
+        myAutos(1) = New Car("Mary", 40, 234)
+        myAutos(2) = New Car("Viper", 40, 34)
+        myAutos(3) = New Car("Mel", 40, 4)
+        myAutos(4) = New Car("Chucky", 40, 5)
+
+        ' Print the name and ID of each car.
+        For Each c As Car In myAutos
+            Console.WriteLine($"Car {c.ID} is named {c.Name}.")
+        Next
+        Console.WriteLine()
+
+        ' Sort my cars?
+        Array.Sort(myAutos)
+
+
+        ' Print the name and ID of each car.
+        Console.WriteLine("-> After sorting")
+        For Each c As Car In myAutos
+            Console.WriteLine($"Car {c.ID} is named {c.Name}.")
+        Next
+
+
+    End Sub
+
+End Module
