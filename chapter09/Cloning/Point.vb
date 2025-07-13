404cc632e51713b9b29445fa6f92c2ce7d125565
David Stevenson <drstevenson1958@yahoo.com>
2025-07-13 14:46:17 -0400

    Chapter 9: Building Cloneable Objects (ICloneable)

---
+++ chapter09/Cloning/Point.vb
@@ -0,0 +1,21 @@
+﻿' A class named Point
+Public Class Point
+    Implements ICloneable
+    Public Property X As Integer
+    Public Property Y As Integer
+
+    Public Sub New()
+    End Sub
+    Public Sub New(ByVal xPos As Integer, ByVal yPos As Integer)
+        X = xPos
+        Y = yPos
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return $"X = {X} ; Y = {Y}"
+    End Function
+
+    Public Function Clone() As Object Implements ICloneable.Clone
+        Return New Point(X, Y)
+    End Function
+End Class
---
+++ chapter09/Cloning/Program.vb
@@ -0,0 +1,19 @@
+﻿Module Program
+
+    Sub Main()
+        Console.WriteLine("***** Fun with ICloneable *****")
+        Dim p1 As New Point(50, 50)
+
+        ' If Option Strict is enabled, you must perform an explicit cast, as Clone()
+        ' returns a System.Object.
+        Dim p2 As Point = CType(p1.Clone(), Point)
+        p2.X = 0
+
+        ' Prints X = 50; Y = 50
+        Console.WriteLine(p1)
+
+        ' Prints X = 0 ; Y = 50
+        Console.WriteLine(p2)
+    End Sub
+
+End Module
