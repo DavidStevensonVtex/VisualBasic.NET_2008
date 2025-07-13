564a71c3e57482b143e96b2db2d497ee022751ae
David Stevenson <drstevenson1958@yahoo.com>
2025-07-13 14:24:50 -0400

    Chapter 9: Designing Interface Hierarchies

---
+++ chapter09/SuperShape/Interfaces.vb
@@ -0,0 +1,13 @@
+﻿Public Interface IDrawable
+    Sub Draw()
+End Interface
+
+Public Interface IPrintable
+    Inherits IDrawable
+    Sub Print()
+End Interface
+
+Public Interface IRenderToMemory
+    Inherits IPrintable
+    Sub Render()
+End Interface
